da91e0634fb0d10f2f8efcc48d384bb295254de7
Marcial Soto <marcialsotosanchez@gmail.com>
2015-08-25 12:24:51 -0500

    Culminado sección de multimedia

---
+++ wp-content/themes/inclusiva/templates/section-multimedia.php
@@ -0,0 +1,86 @@
+<section class="section multimedia__banner">
+	<div class="wrap container" role="document">
+		<div class="content row">
+			<main class="multimedia">
+				<div class="page-header">	
+				<h3>Multimedia</h3>
+				</div>
+				<!-- tabs left -->
+				<div class="tabbable tabs-left">
+					<ul class="nav nav-tabs">
+						<li class="active"><a href="#a" data-toggle="tab"><i class="fa fa-file-image-o visible-xs"></i> <span class="hidden-xs">Fotos</span></a></li>
+						<li><a href="#b" data-toggle="tab"><i class="fa fa-youtube-play visible-xs"></i></i> <span class="hidden-xs">Videos</span></a></li>
+					</ul>
+					<div class="tab-content">
+						<div class="tab-pane active" id="a">
+							<div class="multimedia--1">
+								<figure>
+									<a href="#" class="tab-icon"><i class="fa fa-file-image-o"></i></a>
+									<img class="img-responsive" src="http://lorempixel.com/420/250/sports/1/" />
+									<figcaption>
+										<time>Lunes, 3 de mayo de 2015</time>
+										<h3 class="tab-title"><a href="">Entrevista al Director Ejecutivo en Ayacucho</a></h3>
+									</figcaption>
+								</figure>
+							</div>
+							<div class="multimedia--2">
+								<figure>
+									<a href="#" class="tab-icon"><i class="fa fa-file-image-o"></i></a>
+									<img class="img-responsive" src="http://lorempixel.com/300/200/sports/2/" />
+									<figcaption>
+										<time>Lunes, 3 de mayo de 2015</time>
+										<h3 class="tab-title"><a href="">Entrevista al Director Ejecutivo en Ayacucho</a></h3>
+									</figcaption>
+								</figure>
+								<figure>
+									<a href="#" class="tab-icon"><i class="fa fa-file-image-o"></i></a>
+									<img class="img-responsive" src="http://lorempixel.com/300/200/sports/3/" />
+									<figcaption>
+										<time>Lunes, 3 de mayo de 2015</time>
+										<h3 class="tab-title"><a href="">Entrevista al Director Ejecutivo en Ayacucho</a></h3>
+									</figcaption>
+								</figure>
+							</div>
+							<?php $format_link = get_post_format_link(video); ?>
+						</div>
+
+						<div class="tab-pane" id="b">
+							<div class="multimedia--1">
+								<figure>
+									<a href="#" class="tab-icon"><i class="fa fa-youtube-play"></i></i></a>
+									<img class="img-responsive" src="http://lorempixel.com/420/250/sports/4/" />
+									<figcaption>
+										<time>Lunes, 3 de mayo de 2015</time>
+										<h3 class="tab-title"><a href="">Entrevista al Director Ejecutivo en Ayacucho</a></h3>
+									</figcaption>
+								</figure>
+							</div>
+							<div class="multimedia--2">
+								<figure>
+									<a href="#" class="tab-icon"><i class="fa fa-youtube-play"></i></i></a>
+									<img class="img-responsive" src="http://lorempixel.com/300/200/sports/5/" />
+									<figcaption>
+										<time>Lunes, 3 de mayo de 2015</time>
+										<h3 class="tab-title"><a href="">Entrevista al Director Ejecutivo en Ayacucho</a></h3>
+									</figcaption>
+								</figure>
+								<figure>
+									<a href="#" class="tab-icon"><i class="fa fa-youtube-play"></i></i></a>
+									<img class="img-responsive" src="http://lorempixel.com/300/200/sports/6/" />
+									<figcaption>
+										<time>Lunes, 3 de mayo de 2015</time>
+										<h3 class="tab-title"><a href="">Entrevista al Director Ejecutivo en Ayacucho</a></h3>
+									</figcaption>
+								</figure>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- /tabs -->
+			</main>
+			<aside class="banner">
+				Hola	
+			</aside>
+		</div>
+	</div>
+</section>
