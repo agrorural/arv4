6aa336f4c70d01d0a70d3e42e4433874f369cc68
Marcial Soto <marcialsotosanchez@gmail.com>
2015-09-25 16:44:40 -0500

    Terminado Gobierno Abierto

--- wp-content/themes/inclusiva/templates/content-page-gobierno-abierto.php
+++ wp-content/themes/inclusiva/templates/content-page-gobierno-abierto.php
@@ -14,7 +14,7 @@
 			
 			<div class="caption">
 				<h3>Conoce</h3>
-				<p>Información sobre lo que está haciendo, sobre nuestras políticas y planes.</p>
+				<p>AGRO RURAL da a conocer las acciones que realiza y como se utilizan los recursos públicos, a la vez que promueve la innovación, para lo cual publica la información y los datos que genera a través de herramientas como el Portal de Transparencia Estándar, Portal de Datos Abiertos, sistema de acceso a la información pública, etc.</p>
 				<?php
 				if (has_nav_menu('transparencia_navigation')) :
 				wp_nav_menu(['theme_location' => 'transparencia_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav nav-sidebar']);
@@ -37,8 +37,7 @@
 			
 			<div class="caption">
 				<h3>Participa</h3>
-				<p>Únete y haz sentir tu voz en nuestra comunidad.</p>
-				<?php
+				<p>AGRO RURAL a través de las redes sociales y el uso de plataformas participativas busca implicar a la ciudadanía, servidores civiles, organizaciones de la sociedad civil y otras instituciones públicas para mejorar el diseño e implementación de las políticas públicas para el desarrollo agrario rural.</p>				<?php
 				if (has_nav_menu('participa_navigation')) :
 				wp_nav_menu(['theme_location' => 'participa_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav nav-sidebar']);
 				endif;
@@ -59,7 +58,7 @@
 			?>
 			<div class="caption">
 		   		<h3>Colabora</h3>
-		    	<p>Contribuye con el esfuerzo por trabajar por mejorar el agro en el país.</p>
+		    	<p>AGRO RURAL promueve la colaboración activa con otras instituciones públicas, privadas, sociedad civil y ciudadanos a través de herramientas para mejorar los servicios que proporcionan a los pequeños y medianos agricultores en su ámbito de competencia.</p>
 		    	<?php
 				if (has_nav_menu('colabora_navigation')) :
 				wp_nav_menu(['theme_location' => 'colabora_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav nav-sidebar']);
@@ -81,7 +80,7 @@
 			?>
 			<div class="caption">
 		   		<h3>Desarrolla</h3>
-		    	<p>Brindamos las herramientas que necesitas para abrir datos.</p>
+		    	<p>Compartimos las herramientas que necesitas para usar o reutilizar los datos, asimismo para que puedas poner a disposición de la comunidad tus contribuciones, sea para desarrollar servicios con fines sociales o comerciales.</p>
 		    	<?php
 				if (has_nav_menu('desarrolla_navigation')) :
 				wp_nav_menu(['theme_location' => 'desarrolla_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav nav-sidebar']);
