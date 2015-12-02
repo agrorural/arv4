c27567305a4c00ef3fcc4b1da9b0cb50c5b94188
Marcial Soto <marcialsotosanchez@gmail.com>
2015-12-02 14:22:31 -0500

    Cambio en el home

--- wp-content/plugins/bwp-recaptcha/vendor/kminh/bwp-framework/assets/option-page/js/op.js
+++
@@ -1,9 +0,0 @@
-/*global jQuery,anchors,bwp_common*/
-jQuery(function($) {
-	'use strict';
-
-	// do these once on document ready
-	bwp_common.enhance_form_fields();
-
-	anchors.add('.wrap h3');
-});
