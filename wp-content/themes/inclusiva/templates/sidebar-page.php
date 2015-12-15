984e4027dc61680f25fa50b65e285cfc4c88c307
Marcial Soto <marcialsotosanchez@gmail.com>
2015-12-15 17:17:56 -0500

    Formularios y actualizacion de plugins

--- wp-content/themes/inclusiva/templates/sidebar-page.php
+++ wp-content/themes/inclusiva/templates/sidebar-page.php
@@ -16,6 +16,20 @@
 	endif;
 	?>
 </section>
+<?php } elseif ( is_page( array ( 'Contacto'))){ ?>
+
+<section class="widget text-2 widget_text"><h3>La Institución</h3>			<div class="textwidget"><address>
+  <strong><a href="//localhost:3000/portal">AGRO RURAL</a></strong><br>
+<a href="https://www.google.com.pe/maps/place/AGRO+RURAL/@-12.0796521,-77.0425728,15z/data=!4m2!3m1!1s0x0:0x6f311078be8946fa">Av. Salaverry 1388,<br>
+  Jesús María, Lima 11, PE</a><br>
+  <abbr title="Teléfono">T:</abbr> (511) 205-8030
+</address>
+
+<address>
+  <strong>Contacto</strong><br>
+  <a href="mailto:webmaster@agrorural.gob.pe">webmaster@agrorural.gob.pe</a>
+</address></div>
+		</section>
 
 <?php } elseif ( is_page( array ( 
 									'objetivos', 
