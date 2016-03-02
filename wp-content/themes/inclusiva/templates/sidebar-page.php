e901877d3fd1e6c0d3ea42bf49e9967ae32b982a
Marcial Soto <marcialsotosanchez@gmail.com>
2016-03-02 17:07:42 -0500

    csst

--- wp-content/themes/inclusiva/templates/sidebar-page.php
+++ wp-content/themes/inclusiva/templates/sidebar-page.php
@@ -45,9 +45,22 @@
 				) 
 {?>
     <?php get_template_part('templates/sidebar', 'proyecto-pssa'); ?>
-<?php } else if( is_page( array('csst') ) ) {?>
+<?php } else if( is_page( array('Comité de Seguridad y Salud en el Trabajo de AGRO RURAL') ) ) {?>
 <section class="widget">
-	<h3>Formulario de Requerimientos</h3>
+	<h3>Documentos</h3>
+
+<p>
+<ul>
+<li><a href="http://agrorural.dev/portal/wp-content/uploads/risst/REGLAMENTO INTERNO DE SEGURIDAD Y SALUD EN EL TRABAJO - AGRO RURAL.pdf"><i class="fa fa-file-pdf-o"></i> Reglamento Interno de Seguridad y Salud en el Trabajo</a></li>
+
+<li><a href="http://agrorural.dev/portal/wp-content/uploads/risst/POLITICA_EN_SEGURIDAD_a4.pdf"><i class="fa fa-file-pdf-o"></i> Política en Seguridad</a></li>
+
+<li><a href="http://agrorural.dev/portal/wp-content/uploads/risst/Reglamento_de_la_Ley_N_29783_ley_de_seguridad_y_salud_en_el_trabajo.pdf"><i class="fa fa-file-pdf-o"></i> Reglamento de la Ley N° 29783 Ley de Seguridad y Salud en el Trabajo</a></li>
+
+<li><a href="http://agrorural.dev/portal/wp-content/uploads/risst/D.S. 002-2013-TR POLITICA NACIONAL DE SEGURIDAD Y SALUD EN EL TRABAJO.pdf"><i class="fa fa-file-pdf-o"></i> D.S. 002-2013-TR Política Nacional de Seguridad y Salud en el Trabajo</a></li>
+
+<li><a href="http://agrorural.dev/portal/wp-content/uploads/risst/LEY SEGURIDAD Y SALUD LABORAL.pdf"><i class="fa fa-file-pdf-o"></i> Ley Seguridad y Salud Laboral</a></li>
+</ul>
 </section>
 <?php } else {?>
 	<?php dynamic_sidebar('sidebar-primary'); ?>
