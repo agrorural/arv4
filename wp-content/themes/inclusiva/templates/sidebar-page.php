3698b13971ba2115e59bad652c9e48522dc3b3d8
Marcial Soto <marcialsotosanchez@gmail.com>
2016-03-03 11:56:58 -0500

    Litle bug whit links

--- wp-content/themes/inclusiva/templates/sidebar-page.php
+++ wp-content/themes/inclusiva/templates/sidebar-page.php
@@ -51,15 +51,15 @@
 
 <p>
 <ul>
-<li><a href="http://agrorural.dev/portal/wp-content/uploads/risst/REGLAMENTO INTERNO DE SEGURIDAD Y SALUD EN EL TRABAJO - AGRO RURAL.pdf"><i class="fa fa-file-pdf-o"></i> Reglamento Interno de Seguridad y Salud en el Trabajo</a></li>
+<li><a href="<?php echo bloginfo('url'); ?>/wp-content/uploads/risst/REGLAMENTO INTERNO DE SEGURIDAD Y SALUD EN EL TRABAJO - AGRO RURAL.pdf"><i class="fa fa-file-pdf-o"></i> Reglamento Interno de Seguridad y Salud en el Trabajo</a></li>
 
-<li><a href="http://agrorural.dev/portal/wp-content/uploads/risst/POLITICA_EN_SEGURIDAD_a4.pdf"><i class="fa fa-file-pdf-o"></i> Política en Seguridad</a></li>
+<li><a href="<?php echo bloginfo('url'); ?>/wp-content/uploads/risst/POLITICA_EN_SEGURIDAD_a4.pdf"><i class="fa fa-file-pdf-o"></i> Política en Seguridad</a></li>
 
-<li><a href="http://agrorural.dev/portal/wp-content/uploads/risst/Reglamento_de_la_Ley_N_29783_ley_de_seguridad_y_salud_en_el_trabajo.pdf"><i class="fa fa-file-pdf-o"></i> Reglamento de la Ley N° 29783 Ley de Seguridad y Salud en el Trabajo</a></li>
+<li><a href="<?php echo bloginfo('url'); ?>/wp-content/uploads/risst/Reglamento_de_la_Ley_N_29783_ley_de_seguridad_y_salud_en_el_trabajo.pdf"><i class="fa fa-file-pdf-o"></i> Reglamento de la Ley N° 29783 Ley de Seguridad y Salud en el Trabajo</a></li>
 
-<li><a href="http://agrorural.dev/portal/wp-content/uploads/risst/D.S. 002-2013-TR POLITICA NACIONAL DE SEGURIDAD Y SALUD EN EL TRABAJO.pdf"><i class="fa fa-file-pdf-o"></i> D.S. 002-2013-TR Política Nacional de Seguridad y Salud en el Trabajo</a></li>
+<li><a href="<?php echo bloginfo('url'); ?>/wp-content/uploads/risst/D.S. 002-2013-TR POLITICA NACIONAL DE SEGURIDAD Y SALUD EN EL TRABAJO.pdf"><i class="fa fa-file-pdf-o"></i> D.S. 002-2013-TR Política Nacional de Seguridad y Salud en el Trabajo</a></li>
 
-<li><a href="http://agrorural.dev/portal/wp-content/uploads/risst/LEY SEGURIDAD Y SALUD LABORAL.pdf"><i class="fa fa-file-pdf-o"></i> Ley Seguridad y Salud Laboral</a></li>
+<li><a href="<?php echo bloginfo('url'); ?>/wp-content/uploads/risst/LEY SEGURIDAD Y SALUD LABORAL.pdf"><i class="fa fa-file-pdf-o"></i> Ley Seguridad y Salud Laboral</a></li>
 </ul>
 </section>
 <?php } else {?>
