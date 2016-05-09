e6770e5fa262cad305518da03c2a3b244fdf526a
Marcial Soto <marcialsotosanchez@gmail.com>
2016-05-09 12:43:18 -0500

    Actualización y fix iconos

--- wp-content/themes/inclusiva/templates/content-home-news-list.php
+++ wp-content/themes/inclusiva/templates/content-home-news-list.php
@@ -1,8 +1,19 @@
 <article <?php post_class(); ?>>
+	<?php 
+		global $post;
+		$format = get_post_format();
+		$has__format = has_post_format($format,$post->post_id);
+		$format__link = get_post_format_link( $format );
+	?>
 	<?php if ( has_post_thumbnail() ){ ?>
 		<figure>
+			<?php if ( $has__format ){ ?>
+				<a title="<?php echo 'Contiene '.$format; ?>" href="<?php the_permalink(); ?>" class="format-icon tip">
+					<i class="fa fa-<?php echo $format; ?>"></i>
+				</a>
+			<?php } ?>
 			<a href="<?php the_permalink(); ?>">
-				<?php the_post_thumbnail('thumb-news-list', array('class'=>'img-responsive')); ?>
+				<?php the_post_thumbnail('thumb-news-list', array('class' => 'img-responsive')); ?>
 			</a>
 		</figure>
 	<?php } ?>
