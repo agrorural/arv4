7d08bb775af74e192a30ef4bf4eff1cd0738249b
Marcial Soto <marcialsotosanchez@gmail.com>
2016-05-25 12:48:14 -0500

    Social icons mejorados

--- wp-content/themes/inclusiva/templates/content-single.php
+++ wp-content/themes/inclusiva/templates/content-single.php
@@ -1,33 +1,19 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
-    <header class="col-sm-10">
+    <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
-      <?php if( $post->post_excerpt ) { ?>
-        <div class="entry-summary">
-          <p class="entry-summary"><?php echo $post->post_excerpt; ?></p>  
-        </div>
-      <?php } ?>
     </header>
-    <div class="entry-content">    
-      <div class="entry-content__article">
+    <div class="entry-content">
       <?php if ( has_post_thumbnail() ) { the_post_thumbnail('full', array('class' => 'img-responsive')); } ?>
-        <?php the_content(); ?>
-        <footer>
+      <?php the_content(); ?>
+    </div>
+    <footer>
+      <hr />
+        <?php get_template_part('templates/sharing','list'); ?>
+      <hr />
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
     </footer>
     <?php comments_template('/templates/comments.php'); ?>
-      </div>
-              <div class="entry-content__share">
-        <h5 style="margin-top:0">Compartir</h5>
-        <div class="sharing-list">
-          <ul class="list-unstyled">
-            <li id="fb"><a class="btn btn-block" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>"><i class="fa fa-facebook"></i>Facebook</a></li>
-            <li id="tw"><a class="btn btn-block" href="https://twitter.com/home?status=<?php the_permalink(); ?>"><i class="fa fa-twitter"></i>Twitter</a></li>
-            <li id="gp"><a class="btn btn-block" href="https://plus.google.com/share?url=<?php the_permalink(); ?>"><i class="fa fa-google-plus"></i>Google +</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
   </article>
 <?php endwhile; ?>
--- wp-content/themes/inclusiva/templates/sharing-list.php
+++ wp-content/themes/inclusiva/templates/sharing-list.php
@@ -1,7 +1,40 @@
+<?php 
+	global $post;
+	
+	$post_slug=$post->post_name;
+	$count = json_decode(file_get_contents("https://api.facebook.com/method/fql.query?format=json&query=SELECT+url,normalized_url,total_count,share_count,comment_count,like_count,click_count,commentsbox_count+FROM+link_stat+WHERE+url+%3D+%27http://www.agrorural.gob.pe/".$post_slug."/%27"),true);
+	$share_count = $count[0]['share_count'];
+	//echo $post_slug;
+	//var_dump($count);
+?>
 <div class="sharing-list">
 	<ul class="list-inline">
-		<li id="fb"><a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>"><i class="fa fa-facebook"></i></a></li>
-		<li id="tw"><a href="https://twitter.com/home?status=<?php the_permalink(); ?>"><i class="fa fa-twitter"></i></a></li>
-		<li id="gp"><a href="https://plus.google.com/share?url=<?php the_permalink(); ?>"><i class="fa fa-google-plus"></i></a></li>
+		<li id="fb">
+			<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>">
+				<span class="fa-stack fa-lg">
+				  <i class="fa fa-circle fa-stack-2x"></i>
+				  <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+				  <?php if ($share_count > 0) {?>
+				  	<span class="badge"><?php echo $count[0]['share_count']; ?></span>
+				  <?php } ?>
+				</span>
+			</a>
+		</li>
+		<li id="tw">
+			<a href="https://twitter.com/home?status=<?php the_permalink(); ?>">
+				<span class="fa-stack fa-lg">
+				  <i class="fa fa-circle fa-stack-2x"></i>
+				  <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+				</span>
+			</a>
+		</li>
+		<li id="gp">
+			<a href="https://plus.google.com/share?url=<?php the_permalink(); ?>">
+				<span class="fa-stack fa-lg">
+				  <i class="fa fa-circle fa-stack-2x"></i>
+				  <i class="fa fa-google-plus fa-stack-1x fa-inverse"></i>
+				</span>
+			</a>
+		</li>
 	</ul>
 </div>
