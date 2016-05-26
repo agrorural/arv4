ea538f252a1da716316cb20fab2bda885b91decf
Marcial Soto <marcialsotosanchez@gmail.com>
2016-05-26 12:14:05 -0500

    Favicon

--- wp-content/themes/inclusiva/templates/head.php
+++ wp-content/themes/inclusiva/templates/head.php
@@ -2,5 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri (); ?>/favicon.ico" />
+  <link rel="apple-touch-icon image_src" href="<?php echo get_template_directory_uri (); ?>/favicon.png" />
   <?php wp_head(); ?>
 </head>
