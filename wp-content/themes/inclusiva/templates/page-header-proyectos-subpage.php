cda5ed4543ce2eaed1a7492e7ff7cedab66adf3a
Marcial Soto <marcialsotosanchez@gmail.com>
2016-06-21 12:16:12 -0500

    Fix subpaginas

--- wp-content/themes/inclusiva/templates/page-header-proyectos-subpage.php
+++ wp-content/themes/inclusiva/templates/page-header-proyectos-subpage.php
@@ -8,28 +8,60 @@
 	$pry__logo__txt = get_field('pry__logo__txt', $parent_page_ID);
 	$pry__web = get_field('pry__web', $parent_page_ID); 
 	$pry__tag = get_field('pry__tag', $parent_page_ID); 
+	$pry__fb = get_field('pry__fb', $parent_page_ID); 
+	$pry__tw = get_field('pry__tw', $parent_page_ID); 
+	$pry__yt = get_field('pry__yt', $parent_page_ID); 
 	$tag_by_ID = get_term_by('id', $pry__tag, 'post_tag');
 	$tag__slug = $tag_by_ID->slug;
 ?>
-<div class="media">
-	<div class="container">
-	  <div class="media-left">
-	    <a href="<?php echo the_permalink(); ?>">
-	    <?php if($pry__logo){ ?>
-	      <img class="" src="<?php echo $pry__logo; ?>" alt="<?= Titles\title(); ?>">
-	      <?php if($pry__web && !$pry__logo__txt){ ?>
-				<p><a href="<?php echo $pry__web; ?>" target="_blank" rel="follow"><?php echo $pry__web; ?></a></p>
-			<?php } ?>
-	     <?php } ?>
-	    </a>
-	  </div>
-	  <?php if($pry__logo__txt){ ?>
-		  <div class="media-body">
-		  <h1><?php echo get_the_title( $parent_page_ID );  ?></h1>
-		    	<?php if($pry__web){ ?>
-					<p><a href="<?php echo $pry__web; ?>" target="_blank" rel="follow"><?php echo $pry__web; ?></a></p>
-				<?php } ?>
-		  </div>
+
+<div class="proyecto">
+	<?php if($pry__logo){ ?>
+		<div class="proyecto__logo">
+			<a href="<?php echo the_permalink(); ?>">
+				<img class="img-responsive" src="<?php echo $pry__logo; ?>" alt="<?= Titles\title(); ?>" width="72" height="72">
+			</a>
+		</div>
 	<?php } ?>
-	</div>
+  <?php if($pry__logo__txt){ ?>
+	  <div class="proyecto__descripcion">
+	    <h1><?php echo get_the_title( $parent_page_ID );  ?></h1>
+	  </div>
+  <?php } ?>
+	<?php if( $pry__fb || $pry__tw || $pry__yt || $pry__web ) { ?>
+		  <div class="sharing-list">
+	    	<ul class="list-inline">
+	    		<?php if($pry__fb){ ?>
+					<li id="fb">
+						<a href="<?php echo 'http://www.facebook.com/' . $pry__fb; ?>" class="tip" target="_blank" rel="follow" title="Síguenos en Facebook">
+							<span class="fa-stack fa-lg">
+				              <i class="fa fa-circle fa-stack-2x"></i>
+				              <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+				            </span>
+						</a>
+					</li>
+				<?php } ?>
+				<?php if($pry__tw){ ?>
+					<li id="tw">
+						<a href="<?php echo 'http://twitter.com/' . $pry__tw; ?>" class="tip" target="_blank" rel="follow" title="Síguenos en Twitter">
+							<span class="fa-stack fa-lg">
+				              <i class="fa fa-circle fa-stack-2x"></i>
+				              <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+				            </span>
+						</a>
+					</li>
+				<?php } ?>
+		    	<?php if($pry__web){ ?>
+					<li>
+						<a href="<?php echo $pry__web; ?>" class="tip" target="_blank" rel="follow" title="Visita nuestra Web">
+							<span class="fa-stack fa-lg">
+				              <i class="fa fa-circle fa-stack-2x"></i>
+				              <i class="fa fa-globe fa-stack-1x fa-inverse"></i>
+				            </span>
+						</a>
+					</li>
+				<?php } ?>
+	   		</ul>
+	    </div>
+	<?php } ?>
 </div>
