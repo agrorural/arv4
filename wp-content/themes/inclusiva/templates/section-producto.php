10937e90cbbda8978f7f8f22a8a5f419c1568f7c
Marcial Soto <marcialsotosanchez@gmail.com>
2016-07-06 17:59:46 -0500

    Productos mejorados

--- wp-content/themes/inclusiva/templates/section-producto.php
+++ wp-content/themes/inclusiva/templates/section-producto.php
@@ -1,6 +1,6 @@
 <?php if (is_post_type_archive('producto')) {?>
 	<section class="section clasificacion">
-			<?php 
+		<?php 
 			$args = array(
 					'taxonomy'	=> 'clasificacion',
 					'orderby'	=> 'count',
@@ -10,24 +10,41 @@
 			);
 			$clasificaciones = get_terms( $args ); 
 		?>
-			<?php 
-				//echo '<pre>';
-				//	var_dump($clasificaciones);
-				//echo '</pre>';
+		<?php 
+			//echo '<pre>';
+			//	var_dump($clasificaciones);
+			//echo '</pre>';
 
-				if ( ! empty( $clasificaciones ) && ! is_wp_error( $clasificaciones ) ){
-			
-				    foreach ( $clasificaciones as $clasificacion ) {
-				    	$term_link = get_term_link( $clasificacion );
-				    	if ( is_wp_error( $term_link ) ) {
-					        continue;
-					    }
-				        echo '<section class="thumbnail col-sm-4"><a href="' . esc_url( $term_link ) . '">' . $clasificacion->name . ' <span>('. $clasificacion->count .')</span></a></section>';
+			if ( ! empty( $clasificaciones ) && ! is_wp_error( $clasificaciones ) ){
+		
+			    foreach ( $clasificaciones as $clasificacion ) {
+			    	$term_link = get_term_link( $clasificacion );
+			    	if ( is_wp_error( $term_link ) ) {
+				        continue;
 				    }
-				}
-			?>
+			        echo '<section class="thumbnail col-sm-4"><a href="' . esc_url( $term_link ) . '">' . $clasificacion->name . ' <span>('. $clasificacion->count .')</span></a></section>';
+			    }
+			}
+		?>
 	</section>
 <?php } ?>
-<?php while (have_posts()) : the_post(); ?>
+
+
+<?php
+	global $paged;
+	global $wp_query;
+	$temp = $wp_query; 
+	$wp_query = null; 
+	$wp_query = new WP_Query(); 
+	$wp_query->query('post_type=producto&posts_per_page=12&paged='.$paged);
+	while ($wp_query->have_posts()) : $wp_query->the_post(); 
+?>
   <?php get_template_part('templates/content', 'producto'); ?>
 <?php endwhile; ?>
+
+<?php wp_pagenavi(); ?>
+
+<?php 
+  $wp_query = null; 
+  $wp_query = $temp; 
+?>
