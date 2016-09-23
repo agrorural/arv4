426bc02a0400dc2f346943402714c050ac3775f6
Marcial Soto <marcialsotosanchez@gmail.com>
2016-09-23 15:33:18 -0500

    Dinamizando banners de serviagro

--- wp-content/themes/inclusiva/tribe-events/month.php
+++ wp-content/themes/inclusiva/tribe-events/month.php
@@ -16,11 +16,52 @@ if ( ! defined( 'ABSPATH' ) ) {
 <?php do_action( 'tribe_events_before_template' ) ?>
 <!-- Banners Serviagro -->
 <?php if( is_tax( 'tribe_events_cat', 'serviagro') ) { ?>
-		<div class="wrapper">
-			<div class="banner">
-				<a href="https://issuu.com/serviagro/stacks/3b64c2c809e14c068f1bf043f4b786a9" target="_blank"><img src="<?php echo bloginfo('template_url'); ?>/dist/images/bn__serviagro.png" class="img-responsive" /></a>
-			</div>
-		</div>
+	<?php 
+
+	// Argumentos
+	$args  = array(
+		'post_type' => 'Banners', 
+		'posiciones' => 'Serviagro',
+		'post_per_page' => 2
+	);
+	// the query
+	$the_query = new WP_Query( $args ); ?>
+
+	<?php if ( $the_query->have_posts() ) : ?>
+
+			<!-- pagination here -->
+	<section class="wrapper">
+			<!-- the loop -->
+			<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+				<?php // ACF 
+					$banner__url = get_field('banner__url');
+
+					//var_dump($banner__pop);
+					$hoy = date( 'Ymd', current_time( 'timestamp', 1 ));
+					$banner__vig = get_field('banner__vig');
+					$content = esc_attr(get_the_content());
+
+					if ( $banner__vig && intval ( $banner__vig ) <= intval( $hoy ) ){
+				        change_post_status( $post->ID, 'draft' );
+				    } 
+				?>
+					
+						<figure class="banner">
+								<a href="<?php if ($banner__url) { echo $banner__url; } else { echo bloginfo( 'url' ); } ?>">
+								<?php if ( has_post_thumbnail() ) { the_post_thumbnail('full', array ( 'class' => 'img-responsive' ) ); } ?>
+							</a>
+						</figure>
+					
+			<?php endwhile; ?>
+			<!-- end of the loop -->
+	</section>
+			<!-- pagination here -->
+
+			<?php wp_reset_postdata(); ?>
+			
+	<?php else : ?>
+		<?php /* Silence is Golden */  ?>
+	<?php endif; ?>	
 <?php }	?>
 <!-- Banners Serviagro -->
 <!-- Tribe Bar -->
