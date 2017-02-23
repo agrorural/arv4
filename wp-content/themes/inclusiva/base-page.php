d9e273f8a43eb15b4cfcfb5193dedd4b6381b649
Marcial Soto Sánchez <marcialsotosanchez@gmail.com>
2017-02-23 10:08:20 -0500

    Mejorando el Pack Anticorrupcion

--- wp-content/themes/inclusiva/base-page.php
+++ wp-content/themes/inclusiva/base-page.php
@@ -46,19 +46,30 @@ use Roots\Sage\Wrapper;
           <?php if (is_page('pack-anticorrupcion')){ ?>
             <?php
             $pages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_date', 'sort_order' => 'desc' ) );
-            foreach( $pages as $page ) {    
+            foreach( $pages as $page ) {
+              $post_name = $page->post_name;
               $content = $page->post_content;
+              $image = get_the_post_thumbnail_url($page->ID, 'full'); 
               if ( ! $content ) // Check for empty page
+                continue;
+
+              if ( ! $image ) // Check for empty images
                 continue;
 
               $content = apply_filters( 'the_content', $content );
             ?>
             <div class="inner">
-               <article>
-                  <h2><a href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a></h2>
-                  <div class="entry">
-                    <?php echo $content; ?>
-                  </div>
+               <article class="<?php echo $post_name; ?>" style="background: url(<?php echo $image; ?>) no-repeat top center;  -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">
+               <?php //var_dump($image); ?>
+                 <div class="caption">
+                    <i class="icon fa fa-check-square-o" aria-hidden="true"></i>
+                    <h2>
+                      <a href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a>
+                    </h2>
+                    <div class="entry">
+                      <?php echo $content; ?>
+                    </div>
+                 </div>
                 </article> 
             </div>
               
