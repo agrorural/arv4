65873c1bc6948550387ed452a678a71c9a8f795a
Marcial Soto <marcialsotosanchez@gmail.com>
2017-05-09 10:25:20 -0500

    Formulario AIP

--- wp-content/themes/inclusiva/lib/gf_snippets.php
+++ wp-content/themes/inclusiva/lib/gf_snippets.php
@@ -13,11 +13,24 @@ function populate_num_reclamacion( $value ) {
     return $unique;
 }
 
+add_filter( 'gform_field_value_aip__num', 'populate_aip__num' );
+function populate_aip__num( $value ) {
+    $year = date("Y");
+
+    $form_count = RGFormsModel::get_form_counts(12);
+
+    $unique = $form_count['total'] + 1;
+    $unique = str_pad($unique, 3, '0', STR_PAD_LEFT);
+    $unique = 'AIP-' . $unique . '-' . $year;
+
+    return $unique;
+}
+
 add_filter( 'gform_field_value_fec_reclamacion', 'populate_fec_reclamacion' );
 function populate_fec_reclamacion( $value ) {
-    
+
     $gf_date = date_i18n('d/m/Y', time());
-    
+
     return $gf_date;
 }
 
@@ -25,7 +38,7 @@ add_filter( 'gform_field_value_gf__produ__correo', 'populate_gf__produ__correo' 
 function populate_gf__produ__correo( $value ) {
 	global $post;
 	$productores__terms = get_the_terms( get_the_ID($post->ID), 'productor');
-	
+
 	if( !empty($productores__terms) ) {
 		$produ__term = array_pop($productores__terms);
 		$produ__correo = get_field('produ__correo', $produ__term );
